18daed377b73ed721837637cc0f3abb94f0097b8
Aarif-007 <aarifarsalanddd@gmail.com>
2024-02-07 10:47:44 +0530

    aa

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hello");
+// console.log("hello");
+// var a=12;
+// a=34;
+// console.log(a);
+// console.log("jhfdhgdgh");
+// console.log(a);
+// console.log(a);
+// console.log(typeof(null));
+// console.log(typeof(undefined));
+// console.log(3 - "sonu");
+// console.log(3 - "2");
+// console.log(0 == null);
+// console.log(0 == undefined);
+// console.log(0 === null);
+// let a = 69;
+// if (a >= 90) {
+//     console.log("grade is A+");
+// }
+// else if (a >= 75) {
+//     console.log("grade is A");
+// }
+// else if (a >= 55) {
+//     console.log("grade is B");
+// } else if (a >= 40) {
+//     console.log("grade is C");
+// }
+// else if (a >= 20) {
+//     console.log("grade is D");
+// }
+// else {
+//     console.log("grade is FAIL");
+// }
+// let a=3;
+// a++;
+// a++;
+// ++a;
+// a++;
+// console.log(a);
